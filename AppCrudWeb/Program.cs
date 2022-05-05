7be64a50571fb11d6bb85956f446872247481e4e
LuizDevBr <luizfaraujo.eng@gmail.com>
2022-05-04 23:52:48 -0300

    Criado classe produto e config. string de conexão.

--- AppCrudWeb/Program.cs
+++ AppCrudWeb/Program.cs
@@ -1,7 +1,36 @@
+using AppCrudWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//------------------------------------
+// String de Conexão
+var strConn = @"
+Host=localhost;
+Port=5432;
+Pooling=true;
+Database=CRUD_POSTGRES_TESTE;
+User Id=postgres;
+Password=postgres;
+";
+
+// Para configuração da String de Conexão
+builder.Services.AddEntityFrameworkNpgsql().
+    AddDbContext<Contexto>(options =>
+    options.UseNpgsql(strConn));
+
+// Para instalar (versão mais recente) o Postgres (Npgsql) via comando:
+// Install-Package Npgsql.EntityFrameworkCore.PostgreSQL
+
+// Campos sempre acima do builder.Build()
+//------------------------------------
+
+
+
 
 var app = builder.Build();
 
