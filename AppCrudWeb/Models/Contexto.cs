60fffa717a307d33ea0a09e86ef74e83c1e97681
LuizDevBr <luizfaraujo.eng@gmail.com>
2022-05-04 23:54:05 -0300

    Contexto / Produto . cs

---
+++ AppCrudWeb/Models/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppCrudWeb.Models
+{
+    // Para o DBContext, localizado e instalado a versão mais recente do
+    // EntityFrameworkCore (atual de hoje, 05/2022)
+    public class Contexto : DbContext
+    {
+
+        // Construtor para receber o Options da Configurações que irá colocar no AppSettings
+        // Que é a configuração de conexão com o banco
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+
+        // DBSet
+        // Avisa ao Core quais são os objetos devem ser criados quando executar o Migration
+        // E quais são os objetos que serão gerenciados pelo Core
+        public DbSet<Produto> Produtos { get; set; }
+
+
+
+    }
+}
---
+++ AppCrudWeb/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppCrudWeb.Models
+{
+    [Table("Produto")] // nome da tabela, se for o mesmo da classe não é necessário
+    public class Produto
+    {
+        [Column("Id")] // nome da coluna, se for o mesmo da variável não é necessário
+        [Display(Name = "Código")] // é o que irá aparecer na tela
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+
+
+    }
+}
